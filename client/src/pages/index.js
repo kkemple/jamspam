9308137dce3d657e1af47cfc26b0991516383ec4
Kurt Kemple <kurtiskemple@gmail.com>
2020-09-03 15:41:36 -0400

    basic layout

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Button } from "@chakra-ui/core";
+import React, { useEffect } from "react";
+import { Button, Heading, SimpleGrid, Center, Square } from "@chakra-ui/core";
 import { gql, useMutation, useSubscription } from "@apollo/client";
 
 const PLAY_SOUND = gql`
@@ -20,19 +20,45 @@ const SOUND_PLAYED = gql`
   }
 `;
 
+const sprites = {
+  play: [0, 1000],
+  bop: [1000, 2000],
+  spin: [2000, 3000],
+  swipe: [3000, 4000],
+  console: [4000, 5000],
+  chat: [5000, 6000],
+  spam: [6000, 7000],
+  sing: [7000, 8000],
+  zap: [8000, 9000],
+};
+
 const Page = () => {
-  const { data, loading, error } = useSubscription(SOUND_PLAYED);
+  const { data } = useSubscription(SOUND_PLAYED);
   const [playSound] = useMutation(PLAY_SOUND);
 
-  console.log(data, loading, error);
+  useEffect(() => {
+    console.log(data?.soundPlayed.type);
+  }, [data]);
 
+  const types = Object.keys(sprites);
   return (
-    <>
-      <h1>Play Sound</h1>
-      <Button onClick={() => playSound({ variables: { type: "test" } })}>
-        Play it!
-      </Button>
-    </>
+    <Center minH="100vh" flexDirection="column">
+      <Heading mb="4" fontSize="3xl">
+        JamSpam
+      </Heading>
+      <SimpleGrid spacing="4" columns={Math.sqrt(types.length)}>
+        {types.map((type) => (
+          <Square
+            as={Button}
+            size="120px"
+            key={type}
+            onClick={() => playSound({ variables: { type } })}
+          >
+            {type}
+          </Square>
+        ))}
+      </SimpleGrid>
+    </Center>
   );
 };
 
